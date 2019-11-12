180245cfedf41dc23c48fe5c5e177727b1601a5e
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2019-11-12 13:44:10 +0900

    3

---
+++ src/ex_2.ts
@@ -0,0 +1,115 @@
+export {};
+
+//  2-1
+// -----------------------------------------------
+
+function myFilter<T>(arr: T[], predicate: (arg: T) => boolean) {
+  const result: T[] = [];
+  for (const elm of arr) {
+    if (predicate(elm)) {
+      result.push(elm);
+    }
+  }
+  return result;
+}
+
+const res = myFilter([1, 2, 3, 4, 5], num => num % 2 === 0);
+const res2 = myFilter(["foo", "hoge", "bar"], str => str.length >= 4);
+
+// myFilter([1, 2, 3, 4, 5], str => str.length >= 4);
+
+//  2-2
+// -----------------------------------------------
+
+type Speed = "slow" | "medium" | "fast";
+
+function getSpeed(speed: Speed): number {
+  switch (speed) {
+    case "slow":
+      return 10;
+    case "medium":
+      return 50;
+    case "fast":
+      return 200;
+  }
+}
+
+const slowSpeed = getSpeed("slow");
+const mediumSpeed = getSpeed("medium");
+const fastSpeed = getSpeed("fast");
+
+// getSpeed("veryfast");
+
+//  2-3
+// -----------------------------------------------
+
+declare function addEventListener(
+  event: string,
+  callback: () => void,
+  obj?: { capture?: boolean; once?: boolean; passive?: boolean } | boolean
+): void;
+
+// 使用例
+addEventListener("foobar", () => {});
+addEventListener("event", () => {}, true);
+addEventListener("event2", () => {}, {});
+addEventListener("event3", () => {}, {
+  capture: true,
+  once: false,
+});
+
+// エラー例
+// addEventListener("foobar", () => {}, "string");
+// addEventListener("hoge", () => {}, {
+//   capture: true,
+//   once: false,
+//   excess: true,
+// });
+
+// -----------------------------------------------
+
+function giveId<T>(obj: T): T & { id: string } {
+  const id = "本当はランダムがいいけどここではただの文字列";
+  return {
+    ...obj,
+    id,
+  };
+}
+
+// 使用例
+const obj1: {
+  id: string;
+  foo: number;
+} = giveId({ foo: 123 });
+const obj2: {
+  id: string;
+  num: number;
+  hoge: boolean;
+} = giveId({
+  num: 0,
+  hoge: true,
+});
+
+// エラー例
+// const obj3: {
+//   id: string;
+//   piyo: string;
+// } = giveId({
+//   foo: "bar",
+// });
+
+//  2-5
+// -----------------------------------------------
+import React, { useState } from "react";
+
+declare function useState<S>(
+  initialState: S
+): [S, (state: S | ((arg: S) => S)) => S];
+
+const [numState, setNumState] = useState(0);
+setNumState(3);
+setNumState(state => state + 10);
+const [anotherState, setAnotherState] = useState<number | null>(null);
+setAnotherState(100);
+
+// setNumState("foobar");
---
+++ src/ex_3.ts
@@ -0,0 +1,137 @@
+export {};
+
+//  3-1
+// -----------------------------------------------
+
+function mapFromArray<T, K extends keyof T>(arr: T[], key: K): Map<K, T> {
+  const result = new Map();
+  for (const obj of arr) {
+    result.set(obj[key], obj);
+  }
+  return result;
+}
+
+const data = [
+  { id: 1, name: "John Smith" },
+  { id: 2, name: "Mary Sue" },
+  { id: 100, name: "Taro Yamada" },
+];
+
+const dataMap = mapFromArray(data, "id");
+// mapFromArray(data, "age");
+
+//  3-2
+// -----------------------------------------------
+
+type MyPartial<T> = { [P in keyof T]?: T[P] };
+
+type T1 = MyPartial<{
+  foo: number;
+  bar: string;
+}>;
+
+type T2 = MyPartial<{
+  hoge: {
+    piyo: number;
+  };
+}>;
+// -----------------------------------------------
+
+interface EventPayloads {
+  start: {
+    user: string;
+  };
+  stop: {
+    user: string;
+    after: number;
+  };
+  end: {};
+}
+
+class EventDischarger<E> {
+  emit<Ev extends keyof E>(eventName: Ev, payload: E[Ev]) {}
+}
+
+const ed = new EventDischarger<EventPayloads>();
+ed.emit("start", {
+  user: "user1",
+});
+ed.emit("stop", {
+  user: "user1",
+  after: 3,
+});
+ed.emit("end", {});
+
+// error
+// ed.emit("start", {
+//   user: "user2",
+//   after: 0,
+// });
+// ed.emit("stop", {
+//   user: "user2",
+// });
+// ed.emit("foobar", {
+//   foo: 123,
+// });
+
+//  3-4
+// -----------------------------------------------
+
+type Action =
+  | {
+      type: "increment";
+      amount: number;
+    }
+  | {
+      type: "decrement";
+      amount: number;
+    }
+  | {
+      type: "reset";
+      value: number;
+    };
+
+const reducer = (state: number, action: Action): number => {
+  switch (action.type) {
+    case "increment":
+      return state + action.amount;
+    case "decrement":
+      return state - action.amount;
+    case "reset":
+      return action.value;
+  }
+};
+
+// 使用例
+reducer(100, {
+  type: "increment",
+  amount: 10,
+}) === 110;
+reducer(100, {
+  type: "decrement",
+  amount: 55,
+}) === 45;
+reducer(500, {
+  type: "reset",
+  value: 0,
+}) === 0;
+
+// エラー例
+// reducer(0, {
+//   type: "increment",
+//   value: 100,
+// });
+
+//  3-5
+// -----------------------------------------------
+
+type Func<A, R> = A extends undefined ? (arg?: A) => R : (arg: A) => R;
+
+const f1: Func<number, number> = num => num + 10;
+const v1: number = f1(10);
+const f2: Func<undefined, number> = () => 0;
+const v2: number = f2();
+const v3: number = f2(undefined);
+
+// エラー例
+// const v4: number = f1();
